5abb5de519db5be0798357bcadfb30807128a1c3
angwanghyeon <agh1594@naver.com>
2024-05-14 13:48:04 +0900

    next 수정

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   async rewrites(){
       return [
         {
-          source : '/api/:path*',
-          destination : 'http://localhost:8001/api/:path*'
+          source : '/:path*',
+          destination : 'http://localhost:8001/:path*'
         },
 
       ]
